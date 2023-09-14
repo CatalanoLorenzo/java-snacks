93311b09650a323c9279ac1234b451d7a8352cdf
CatalanoLorenzo <lorenzocatalano1995@outlook.it>
2023-09-14 11:39:42 +0200

    snack 1

---
+++ src/org/lessons/java/snack1.java
@@ -0,0 +1,23 @@
+package org.lessons.java;
+
+import java.util.Scanner;
+
+public class snack1 {
+	//Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("inserisci un numero :");
+		
+		int numero = input.nextInt();
+		
+		boolean isPair = numero %2 == 0;
+		
+		System.out.println(
+				isPair
+				? numero
+						: numero + 1
+				);
+	}
+}
