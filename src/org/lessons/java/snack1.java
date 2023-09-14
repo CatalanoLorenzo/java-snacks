9e93eaf007368a3420bf174e9ddd0bd061299c52
CatalanoLorenzo <lorenzocatalano1995@outlook.it>
2023-09-14 11:55:15 +0200

    snack 2

--- src/org/lessons/java/snack1.java
+++ src/org/lessons/java/snack1.java
@@ -3,7 +3,9 @@ package org.lessons.java;
 import java.util.Scanner;
 
 public class snack1 {
+	
 	//Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
+	
 	public static void main(String[] args) {
 		
 		Scanner input = new Scanner(System.in);
--- src/org/lessons/java/snack2.java
+++ src/org/lessons/java/snack2.java
@@ -1,5 +1,31 @@
 package org.lessons.java;
 
+import java.util.Random;
+
 public class snack2 {
 
+	// Generatore di “nomi cognomi” casuali: il Grande Gatsby ha una lista di nomi e
+	// una lista di cognomi, e da queste vuole mostrare a video una falsa lista di
+	// invitati con nome e cognome.
+
+	public static void main(String[] args) {
+		
+		String[] listaNomi = {"Lorenzo","Giuseppe","Luca","Mattia","Domiziano"};
+		
+		String[] listaCognomi = {"Catalano","Sciacca","Macedone","Volpe","De Santis"};
+		
+		for (int i = 0; i < 10; i++) {
+			
+			Random random = new Random();
+			
+			int nomeRandom = random.nextInt(listaNomi.length);
+			
+			int cognomeRandom = random.nextInt(listaCognomi.length);
+			
+			System.out.println(listaNomi[nomeRandom] + " " + listaCognomi[cognomeRandom]);
+			
+			
+		}
+
+	}
 }
